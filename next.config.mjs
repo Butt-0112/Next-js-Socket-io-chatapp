ae26d5a7446794122037ccce749442c91383a84e
Butt-0112 <qasimbuttqb360@gmail.com>
2025-06-01 22:39:57 +0500

    backend url updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_SOCKET_BACKEND_URL: "https://chatapp.echoplay.site",
+        NEXT_PUBLIC_SOCKET_BACKEND_URL: "https://livechat-ae9c.onrender.com",
         NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_Y2xlYW4tZWdyZXQtODcuY2xlcmsuYWNjb3VudHMuZGV2JA",
         CLERK_SECRET_KEY: "sk_test_Noilxo1PCOKjRRJ3AuEAunNjN1TAxLnksjQqpE18XK",
         NEXT_PUBLIC_API_KEY: "AIzaSyA1dQcha91rNqdBOdus07FGtBDp4PTCmW8",
