a9439b9f63ca57503909e739c8b227fadf9127a9
Butt-0112 <qasimbuttqb360@gmail.com>
2025-06-18 00:04:35 +0500

    env updated

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_SOCKET_BACKEND_URL: "https://livechat-ae9c.onrender.com",
         NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_Y2xlYW4tZWdyZXQtODcuY2xlcmsuYWNjb3VudHMuZGV2JA",
-        CLERK_SECRET_KEY: "sk_test_Noilxo1PCOKjRRJ3AuEAunNjN1TAxLnksjQqpE18XK",
+        // CLERK_SECRET_KEY: "sk_test_Noilxo1PCOKjRRJ3AuEAunNjN1TAxLnksjQqpE18XK",
         NEXT_PUBLIC_API_KEY: "AIzaSyA1dQcha91rNqdBOdus07FGtBDp4PTCmW8",
         NEXT_PUBLIC_AUTH_DOMAIN: "livechat-4ea6c.firebaseapp.com",
         NEXT_PUBLIC_PROJECT_ID: "livechat-4ea6c",
@@ -11,7 +11,7 @@ const nextConfig = {
         NEXT_PUBLIC_MESSAGING_SENDER_ID: "148298278052",
         NEXT_PUBLIC_APP_ID: "1:148298278052:web:3147c31ee38c288eaa3231",
         NEXT_PUBLIC_MEASUREMENT_ID: "G-KD8T2X94Z7",
-        SECRET_KEY:"$2b$30$OhcFx2.wPW1BpiWsou0Hce"
+        // SECRET_KEY:"$2b$30$OhcFx2.wPW1BpiWsou0Hce"
     },
     images:{
         domains:['img.clerk.com']
