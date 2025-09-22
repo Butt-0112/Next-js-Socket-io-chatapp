3ce9dc2208346b88cf573ef37af02f086402d2f4
Butt-0112 <qasimbuttqb360@gmail.com>
2025-09-22 21:15:06 +0500

    updated manifest

--- app/layout.js
+++ app/layout.js
@@ -14,14 +14,23 @@ export default function RootLayout({ children }) {
   const [isOpen, setIsOpen] = useState(true)
   const pathname = usePathname(); // Get the current path
   const [isSingleInstance, setIsSingleInstance] = useState(true);
-   
-  
+
+
 
   // List of routes where the sidebar should be hidden 
   const noSidebarRoutes = ['/login', '/signup', '/dashboard'];
 
   const showSidebar = !noSidebarRoutes.some(route => pathname.startsWith(route));
+  useEffect(() => {
+    const preventScroll = (e) => e.preventDefault();
 
+    // prevent touch scrolling on everything except messages container
+    document.addEventListener('touchmove', preventScroll, { passive: false });
+
+    return () => {
+      document.removeEventListener('touchmove', preventScroll);
+    };
+  }, []);
   return (
     <ClerkProvider appearance={{
       baseTheme: dark,
@@ -41,14 +50,14 @@ export default function RootLayout({ children }) {
               disableTransitionOnChange
             >
               <SidebarProvider >
-                {showSidebar  && <AppSidebar />}
+                {showSidebar && <AppSidebar />}
                 {/* {showSidebar && <SidebarTrigger onClick={() => { setIsOpen(!isOpen) }} className={` absolute left-0 z-50 top-4 ${!isOpen && ''} `} />} */}
                 <main className="w-full">
 
                   {
                     // !isSingleInstance ? <div className="text-center">Another instance of the application is already running.</div>
-                      // : 
-                      children
+                    // : 
+                    children
                   }
                 </main>
               </SidebarProvider>
